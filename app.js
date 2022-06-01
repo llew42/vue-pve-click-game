d9d5f878a5b1a3888345b6c7d4999e84cfbadba7
Justin Llewellyn <jllew24@gmail.com>
2022-05-31 23:17:01 -0700

    win/loss logic, special atk logic. relatively rudimentary whatever

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ function getRandomVal(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 function gameWinner(winner) {
-	if(winner === 'player') {
-		alert('Player won!');
-	} else if (winner === 'monster') {
-		alert('Monster won!');
-	}
+	// if(winner === 'player') {
+	// 	alert('Player won!');
+	// } else if (winner === 'monster') {
+	// 	// alert('Monster won!');
+	// }
 }
 
 const app = Vue.createApp({
@@ -17,38 +17,40 @@ const app = Vue.createApp({
 			atkCounter: 0,
 			specialAtkAvailable: true,
 			battleLog: [],
-			winner: '',
+			winner: null,
 		};
 	},
 	methods: {
 		attackMonster() {
 			const attackVal = getRandomVal(5, 12);
 			this.monsterHealth -= attackVal;
-			this.playerBuildUp();
 			this.attackPlayer();
 		},
 		attackPlayer() {
 			const attackVal = getRandomVal(8, 15);
 			this.playerHealth -= attackVal;
+			this.playerSpecialAtkBuildUp();
 		},
 		heal() {
-			const healVal = getRandomVal(5, 15);
-			this.playerHealth += healVal;
-			if(this.playerHealth > 100) {
+			const healVal = getRandomVal(8, 20);
+			console.log('heal val ', healVal);
+			if(this.playerHealth + healVal > 100) {
 				this.playerHealth = 100;
+			} else {
+				this.playerHealth += healVal;
 			}
+			this.attackPlayer();
 		},
 		specialAttack() {
 			const specialAtkVal = getRandomVal(10, 30);
 			this.monsterHealth -= specialAtkVal;
-			this.atkCounter = 0;
 			this.specialAtkAvailable = true;
 			this.attackPlayer();
+			this.atkCounter = 0;
 		},
-		playerBuildUp() {
+		playerSpecialAtkBuildUp() {
 			this.atkCounter++;
-			console.log('atkCounter is ', this.atkCounter)
-			if(this.atkCounter === 3) {
+			if(this.atkCounter % 3 !== 0) {
 				this.specialAtkAvailable = false;
 				console.log('specialAtkAvailable is ', this.specialAtkAvailable);
 			}
@@ -62,12 +64,20 @@ const app = Vue.createApp({
 			return {width: this.playerHealth + '%'};
 		},
 		mayUseSpecialAtk() {
-			return this.atkCounter % 3 !== 0;
+			console.log('mayUseSpecialAtk triggered');
+			if(this.atkCounter % 3 !== 0 || this.atkCounter === 0) {
+				console.log('this.atkCounter === ', this.atkCounter)
+				return this.specialAtkAvailable = true;
+			} else return this.specialAtkAvailable = false;
 		}
 	},
 	watch: {
-		playerHealth() {
-			if(this.playerHealth <= 0) {
+		playerHealth(value) {
+			if(value <= 0 && this.monsterHealth <= 0) {
+				this.playerHealth = 0;
+				this.monsterHealth = 0;
+				this.winner = 'draw';
+			} else if (value <= 0) {
 				this.playerHealth = 0;
 				this.winner = 'monster';
 				const that = this;
@@ -76,8 +86,12 @@ const app = Vue.createApp({
 				}, 1000)
 			}
 		},
-		monsterHealth() {
-			if(this.monsterHealth <= 0) {
+		monsterHealth(value) {
+			if(value <= 0 && this.playerHealth <= 0) {
+				this.playerHealth = 0;
+				this.monsterHealth = 0;
+				this.winner = 'draw';
+			} else if (value <= 0) {
 				this.monsterHealth = 0;
 				this.winner = 'player';
 				const that = this;
